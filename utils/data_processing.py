d6c7eac1731d06128c813e2dd16383acfac0dba6
lanfear <miguel.skim@gmail.com>
2018-05-10 17:09:08 +0100

    Update before moving

--- utils/data_processing.py
+++ utils/data_processing.py
@@ -1,12 +1,11 @@
-import os, glob
+import os
 import numpy as np
 import scipy.io as sio
 import cv2
-import face_detection.detect_face as mtcnn
-import tensorflow as tf
+import face_detection.detect_face_batch as mtcnn
 
-def processDataset(old_dataset_image_path, old_dataset_landmark_path, new_dataset_path,
-                            scale_factor, image_extension='jpg'):
+def process_dataset(old_dataset_image_path, old_dataset_landmark_path, new_dataset_path,
+                            scale_factor, image_extension='jpg', final_image_size=(450, 450)):
     '''
     Detects and crops faces in images provided in 'old_dataset_image_path', applying
     'scale_factor' to the bounding box found, and stores cropped image in
@@ -14,72 +13,68 @@ def processDataset(old_dataset_image_path, old_dataset_landmark_path, new_datase
     and stores the new offsets (relative to the new bounding boxes) also in
     'new_dataset_path'
     
-    Detects face using a MTCNN detector, see
-    'https://arxiv.org/abs/1604.02878'
-    Implementation by David Sandberg, see
-    'https://github.com/davidsandberg/facenet'
-    
     :param old_dataset_image_path: (str) Full path to dataset images
     :param old_dataset_landmark_path: (str) Full path to corresponding landmark files
     :param new_dataset_path: (str) Full path to store processed dataset images
     :param scale_factor: (float) Multiplies the square face bounding boxes found
     by the detector ie. if bounding box is 100 pixels wide and 'scale_factor' is 2 it becomes
     200 pixels wide. Fills cropped box with black pixels where scaled box goes out of image bounds
-    :param image_extension: (str) Image extension. Defaults to 'jpg'
+    :param image_extension: (str, optional) Image extension. Defaults to 'jpg'
+    :param final_image_size: (tuple of ints) Final desired image size in (x dim in pixels, y dim in pixels).
+    All images are resized to 'final_image_size'
+    Will process all images if nothing is specified. Useful for debugging
     :return: --
     '''
 
     '''
-    Load old images and landmarks
+    Load old images, get their detected faces (bboxes) and landmarks (pre-processing)
     '''
+    # Detect face bounding boxes (bboxes) in preprocessed images
 
+    (image_bboxes, image_names) = mtcnn.detect_face_batch_from_directory(old_dataset_image_path,
+                                                                         image_extension=image_extension)
 
-    imageNames = glob.glob(old_dataset_image_path + '/*.' + image_extension)
-    imageNamesNoExtension = [n.split('.')[0] for n in imageNames]  # Get filenames without extension
-    landmarkNames =[imgName + '_pts.mat' for imgName in imageNamesNoExtension] # Only grab landmarks for which there is
-    # a corresponding image
+    landmark_names =[img_name.split('.')[-2] + '_pts.mat' for img_name in image_names] # Only grab landmarks
+    # for which there is a corresponding image
 
-    images = []
-    landmarks = []
+    preprocessed_images = []
+    preprocessed_landmarks = []
 
-    for img, lm in zip(imageNames[:25], landmarkNames[:25]):
-        images.append(cv2.imread(img + '.' + image_extension)) # Load the image
-        landmarks.append(sio.loadmat(old_dataset_landmark_path + '/' + lm)['pts_2d'])
+    # Load images for which faces were found and corresponding landmarks
 
-
-    '''
-    Initialize face detector
-    '''
-
-    sess = tf.Session()
-
-    # Create the 3 cascade networks
-    pnet, rnet, onet = mtcnn.create_mtcnn(sess, None)
-
-    # Set cascade parameters
-    minsize = 40  # Minimum window size to be detected in pixels
-    threshold = [.6, .7, .7]  # Quality threshold for all cascade levels
-    factor = .709 # Image pyramid scaling factor
-
-    # To store cropped images and adjusted landmarks
-    cropped_images = []
-    adjusted_landmarks = []
+    for img, lm in zip(image_names, landmark_names):
+        preprocessed_images.append(cv2.imread(old_dataset_image_path + img))
+        preprocessed_landmarks.append(sio.loadmat(old_dataset_landmark_path + lm)['pts_2d'])
 
     '''
-    Detect faces, crop images, adjust landmarks
+    Crop images and adjust landmarks
     '''
 
-    for img, lm in zip(images, landmarks):
+    for img, bboxes, lm, img_name, lm_name in zip(preprocessed_images, image_bboxes, preprocessed_landmarks,
+                                                  image_names, landmark_names):
 
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # MTCNN trained on RGB images
+        img_height, img_width, _channels = img.shape
 
-        # Run MTCNN detector
-        # Output 'faces' is a n-by-m matrix where n is the num of faces detected and m=5 has bounding box info for each
-        # face in the format xmin, ymin, xmax, ymax, confidence
-        faces, _ = mtcnn.detect_face(img, minsize, pnet, rnet, onet, threshold, factor)
+        #   Get detected face that overlaps the most with provided landmarks. If no face overlaps with more than 2/3
+        #   of landmarks, the image is discarded and not considered suitable for training. This is an heuristic for
+        #   cleaning the dataset
 
-        # Get highest confidence face for this image
-        bbox = faces[np.argmax([faces[:,-1]]),:-1].astype('int32')
+        num_faces_found, _ = bboxes.shape
+
+        num_landmarks_inside_face = np.zeros(num_faces_found) # Will hold num landmarks inside each face in current img
+
+        for i, face in enumerate(bboxes):
+            for lm_point in lm:
+                if lm_point[0] <= face[2] and lm_point[0] >= face[0] and lm_point[1] <= face[3] and lm_point[1] >= face[1]:
+                    num_landmarks_inside_face[i] += 1
+
+        if max(num_landmarks_inside_face) <= 2/3 * len(lm): # If no detected face covers more than 2/3 of landmarks
+            print('\nWARNING: No suitable face bounding box was detected when processing image ' + img_name)
+            continue
+
+        chosen_face_number = np.argmax(num_landmarks_inside_face)
+
+        bbox = bboxes[chosen_face_number,:-1].astype('int32')
 
         # Square the found box coordinates
         width = bbox[2]-bbox[0]
@@ -87,25 +82,30 @@ def processDataset(old_dataset_image_path, old_dataset_landmark_path, new_datase
         side = np.max([width, height])
 
         if width>height:
-            bbox[1] -= side/2
-            bbox[3] += side/2
+            bbox[1] -= (side - height)/2
+            bbox[3] += (side - height)/2
         else:
-            bbox[0] -= side/2
-            bbox[2] += side/2
+            bbox[0] -= (side - width)/2
+            bbox[2] += (side - width)/2
 
         # Scale box
-        bbox[0] -= side/2 * (scale_factor - 1)
-        bbox[2] += side/2 * (scale_factor - 1)
-        bbox[1] -= side/2 * (scale_factor - 1)
-        bbox[3] += side/2 * (scale_factor - 1)
+        final_box_size = int(side * scale_factor)
+        dist_to_offset_after_scaling = int((final_box_size - side)/2)
 
-        # Check if box goes outside image
-        img_height, img_width = img.shape
+        bbox[0] -= dist_to_offset_after_scaling
+        bbox[2] += dist_to_offset_after_scaling
+        bbox[1] -= dist_to_offset_after_scaling
+        bbox[3] += dist_to_offset_after_scaling
+
+        #   Check if box goes outside image
+
+        # Placeholder for cropped image, all channels 0 (black) to start
+        cropped_img = np.zeros((final_box_size, final_box_size, 3))
 
         x_origin_offset = 0
         y_origin_offset = 0
-        x_end_offset = 0
-        y_end_offset = 0
+        x_end_offset = final_box_size
+        y_end_offset = final_box_size
         x_min_crop = bbox[0]
         y_min_crop = bbox[1]
         x_max_crop = bbox[2]
@@ -113,51 +113,67 @@ def processDataset(old_dataset_image_path, old_dataset_landmark_path, new_datase
 
         if bbox[0] < 0:
             x_min_crop = 0
-            x_origin_offset = bbox[0]
+            x_origin_offset = -bbox[0]
         if bbox[1] < 0:
             y_min_crop = 0
-            y_origin_offset = bbox[1]
+            y_origin_offset = -bbox[1]
         if bbox[2] > img_width:
             x_max_crop = img_width
-            x_end_offset = bbox[2] - img_width
+            x_end_offset = final_box_size - (bbox[2] - img_width)
         if bbox[3] > img_height:
             y_max_crop = img_height
-            y_end_offset = bbox[3] - img_height
+            y_end_offset = final_box_size - (bbox[3] - img_height)
 
-        # Crop image
-        cropped_img = np.zeros((side * scale_factor, side * scale_factor)) # holder for cropped img
-        cropped_img[y_origin_offset:-y_end_offset, x_origin_offset:-x_end_offset] = img[y_min_crop:y_max_crop,
+        #   Crop image
+
+        # Correct off-by-1 pixel error due to rounding errors during scaling up of square face patch
+        # (scale is specified as a float)
+        if y_max_crop - y_min_crop  < y_end_offset - y_origin_offset:
+            y_end_offset -= 1
+        if x_max_crop - x_min_crop  < x_end_offset - x_origin_offset:
+            x_end_offset -= 1
+        if y_max_crop - y_min_crop  > y_end_offset - y_origin_offset:
+            y_max_crop -= 1
+        if x_max_crop - x_min_crop  > x_end_offset - x_origin_offset:
+            x_max_crop -= 1
+
+        cropped_img[y_origin_offset:y_end_offset, x_origin_offset:x_end_offset] = img[y_min_crop:y_max_crop,
                                                                                     x_min_crop:x_max_crop]
+
+        # Resize image
+        initial_side_y, initial_side_x, _ = cropped_img.shape
+        scale_factor_x = final_image_size[0] / initial_side_x
+        scale_factor_y = final_image_size[1] / initial_side_y
+
+        cropped_img = cv2.resize(cropped_img, (0,0), fx=scale_factor_x, fy=scale_factor_y)
 
         # Adjust landmarks
         lm[:,0] -= bbox[0]
         lm[:,1] -= bbox[1]
+        lm[:,0] *= scale_factor_x
+        lm[:,1] *= scale_factor_y
 
-        cropped_images.append(cropped_img)
-        adjusted_landmarks.append(lm)
+        '''
+        Save cropped face and adjusted landmark
+        '''
 
-    '''
-    Save cropped faces and adjusted landmarks
-    '''
-
-    # Save in specified directory
-    os.chdir(new_dataset_path)
-    for cropped_img, adjusted_lm, imgNameNoExtension in zip(cropped_images, adjusted_landmarks, imageNamesNoExtension[:25]):
-        cv2.imwrite(imgNameNoExtension + '.' + image_extension, cropped_img)
-        sio.savemat(imgNameNoExtension + '_pts.mat', dict([('pts_2d', adjusted_lm)]))
+        cv2.imwrite(new_dataset_path + img_name, cropped_img)
+        sio.savemat(new_dataset_path + lm_name, dict([('pts_2d', lm)]))
 
 def main():
     '''
-    Testing for processDataset
+    Testing for process_dataset
     :return: --
     '''
+    os.chdir('..')
+    curr_dir = os.getcwd()
 
-    old_dataset_image_path = '/Users/miguelrita/Documents/Paper Implementations/esr/data/AFW'
-    old_dataset_landmark_path = '/Users/miguelrita/Documents/Paper Implementations/esr/data/landmarks/AFW'
-    new_dataset_path = '/Users/miguelrita/Documents/Paper Implementations/esr/processedDataset'
-    scale_factor = 1.1
+    old_dataset_image_path = curr_dir + '/data/AFW/'
+    old_dataset_landmark_path = curr_dir + '/data/landmarks/AFW/'
+    new_dataset_path = curr_dir + '/processed_data/AFW/'
+    scale_factor = 1.5
 
-    processDataset(old_dataset_image_path, old_dataset_landmark_path, new_dataset_path, scale_factor)
+    process_dataset(old_dataset_image_path, old_dataset_landmark_path, new_dataset_path, scale_factor)
 
 if __name__ == '__main__':
     main()
--- utils/visualizer.py
+++ utils/visualizer.py
@@ -2,47 +2,46 @@ import os, glob
 import numpy as np
 import scipy.io as sio
 import cv2
+import face_detection.detect_face_batch as mtcnn
 
-def visualizer(images, landmarks, landmarks2 = np.array([])):
+def visualizer(images, bboxes=None, landmarks=None):
     '''
-    Helper function to visualize precomputed landmarks on a series of images
-    Can visualize 2 sets of landmarks per image
-    Press 'S' to toggle landmarks on/off
+    Helper function to visualize precomputed landmarks and/or detected bounding boxes on a series of images
+    Press 'S' to toggle landmarks/bboxes on/off
     Press 'A'/'D' for previous/next image
     Press 'Q' to quit visualizer
       
     :param images: (list of np arrays) list of BGR input images to visualize
+    :param bboxes: (list of np arrays) list of faces found per image (optional)
     :param landmarks: (list of np arrays) list of landmark matrices, where the
-    number of rows per matrix equals the number of landmarks and the number of columns is 2, for x and y
-    :param landmarks2: optional second list of landmarks per image, same as above
+    number of rows per matrix equals the number of landmarks and the number of columns is 2, for x and y (optional)
     :return: --
     '''
 
     # Cosmetic parameters
     landmarkRadius = 2
-    landmarkColor1 = (0,255,0)
-    landmarkColor2 = (255,0,0)
+    landmarkColor1 = (0, 255, 0)
+    bbox_color = (0, 0, 255)
+    bbox_thickness = 2
 
     # Flow control params
     visualizerRunning = True
     currentImageNum = 0
 
     isDrawingOn = True
-    if len(landmarks2) == 0:
-        drawSecondSetLandmarks = False # No second set of landmarks was provided
-    else:
-        drawSecondSetLandmarks = True
 
     while visualizerRunning:
         currentSourceImage = images[currentImageNum]
         currentImage = np.copy(currentSourceImage) # Will have edits ie. landmarks drawn on top
 
         if isDrawingOn:
-            for lm in landmarks[currentImageNum]:
-                currentImage = cv2.circle(currentImage, (lm[0], lm[1]), landmarkRadius, landmarkColor1, -1)
-        if isDrawingOn and drawSecondSetLandmarks:
-            for lm in landmarks2[currentImageNum]:
-                currentImage = cv2.circle(currentImage, (lm[0], lm[1]), landmarkRadius, landmarkColor2, -1)
+            if not landmarks is None:
+                for lm in landmarks[currentImageNum]:
+                    currentImage = cv2.circle(currentImage, (lm[0], lm[1]), landmarkRadius, landmarkColor1, -1)
+            if not bboxes is None:
+                for face in bboxes[currentImageNum].astype('int32'):
+                    currentImage = cv2.rectangle(currentImage, (face[0], face[1]), (face[2], face[3]), bbox_color,
+                                             bbox_thickness)
 
         if not isDrawingOn:
             cv2.imshow('Visualizer', currentSourceImage)
@@ -61,33 +60,75 @@ def visualizer(images, landmarks, landmarks2 = np.array([])):
         elif key == ord('q'):  # Quit visualization
             visualizerRunning = False
 
-def main():
+
+
+def test1():
     '''
-    Visualizer testing function 
+    Visualizer testing function 1
     '''
 
-    # Load a couple of images and respective landmarks for testing
+    # Load a couple of image paths
 
     numImages = 1000
-    datasetName = 'AFW' # pick numImages first images from this dataset
-    imageExtension = 'jpg'
+    datasetName = 'AFW/' # pick numImages first images from this dataset
+    imageExtension = '.jpg'
 
     os.chdir('../data/'+datasetName) # switch to dataset dir
-    imageNames = glob.glob('*.'+imageExtension)
+    imageNames = glob.glob('*'+imageExtension)
     imageNames = [n.split('.')[0] for n in imageNames] # Get filenames without extension
+
+    images = []
+
+    for img in imageNames[:numImages]:
+        images.append(cv2.imread(img+imageExtension)) # Load the image
+
+    # Detect faces in images
+    faces, names = mtcnn.detect_face_batch(image_list=images, image_names=imageNames[:numImages],
+                                           are_images_rgb=False)
+
+    landmarks = []
+    images_with_faces = []
+
+    # Load images and landmarks for images with detected faces
+    os.chdir('..')
+    for img_name in names:
+        # Load image
+        images_with_faces.append(cv2.imread(datasetName+img_name+imageExtension))
+        # Load landmarks
+        landmarks.append(sio.loadmat('landmarks/'+datasetName+img_name+'_pts.mat')['pts_2d'])
+
+    # Compute data set mean shape
+    meanShape = np.mean(landmarks, axis=0)
+
+    visualizer(images_with_faces, faces, landmarks)
+
+def test2():
+    '''
+    Visualizer testing function 2
+    '''
+
+    # Load processed images and landmarks
+
+    datasetName = 'AFW/' # pick numImages first images from this dataset
+    imageExtension = '.jpg'
+
+    os.chdir('../processed_data/'+datasetName) # switch to dataset dir
+    image_names = glob.glob('*'+imageExtension)
+    image_names = [n.split('.')[0] for n in image_names] # Get filenames without extension
 
     images = []
     landmarks = []
 
-    for img in imageNames[:numImages]:
-        images.append(cv2.imread(img+'.'+imageExtension)) # Load the image
-        landmarks.append(sio.loadmat('../landmarks/'+datasetName+'/'+img+'_pts.mat')['pts_2d'])
+    for img_name in image_names:
+        # Load image
+        images.append(cv2.imread(img_name+imageExtension))
+        # Load landmarks
+        landmarks.append(sio.loadmat(img_name+'_pts.mat')['pts_2d'])
 
-    # Compute data set mean shape and provide it as second landmark set (constant, in this case)
+    # Compute data set mean shape
     meanShape = np.mean(landmarks, axis=0)
-    landmarks2 = [meanShape for lm in landmarks]
 
-    visualizer(images, landmarks, landmarks2)
+    visualizer(images, None, landmarks)
 
 if __name__ == '__main__':
-    main()
+    test2()
